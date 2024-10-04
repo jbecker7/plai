327e6966f190da0ac7acd60a0bce06b08df69c66
Jonathan Becker <76632760+jbecker7@users.noreply.github.com>
2024-10-04 02:40:32 -0400

    Up to pg. 57 with some slight modifications

--- plai.rkt
+++ plai.rkt
@@ -2,16 +2,24 @@
 
 (define-type Exp
   [numE (n : Number)]
-  [boolE (b : Boolean)]
   [plusE (left : Exp) (right : Exp)]
-  [cndE (test : Exp) (then : Exp) (else : Exp)])
+  [varE (name : Symbol)]
+  [let1E (var : Symbol)
+         (value : Exp)
+         (body : Exp)])
 
 (define-type Value
   [numV (the-number : Number)]
   [boolV (the-boolean : Boolean)])
 
+; (boolean-decision (Value -> Value))
+(define (boolean-decision v)
+  (type-case Value v
+    [(boolV b) b]
+    [else (error 'if "expects conditional to evaluate to a boolean")]))
+
 ; Add ONLY deals with values
-; (add: (Value, Value -> Value)
+; (add: (Value, Value -> Value))
 (define (add v1 v2)
   (type-case Value v1
     [(numV n1)
@@ -20,16 +28,35 @@
        [else (error '+ "expects RHS to be a number")])]
     [else (error '+ "expects LHS to be a number")]))
 
-; E -> V
-; Calc takes an expression and evaluates it to a value
-(define (calc e)
-  (type-case Exp e
-    [(numE n) (numV n)]
-    [(boolE b) (boolV b)]
-    [(plusE l r) (add (calc l) (calc r))]
-    [(cndE c t e) (numV 0)])) ; For now, cndE just returns 0
 
-; parse: s-exp -> Exp
+
+(define-type-alias Env (Hashof Symbol Value))
+(define mt-env (hash empty))
+
+(define (lookup (s : Symbol) (n : Env))
+  (type-case (Optionof Value) (hash-ref n s)
+    [(none) (error s "not bound")]
+    [(some v) v]))
+
+; (extend: (Env Symbol Value -> Env))
+(define (extend old-env new-name value)
+  (hash-set old-env new-name value))
+
+
+;; (interp : (Exp Env -> Value))
+(define (interp e nv)
+  (type-case Exp e
+    [(numE n) (numV n)] ; Return a numV instead of raw number (why did the textbook do n???)
+    [(varE s) (lookup s nv)]
+    [(plusE l r) (add (interp l nv) (interp r nv))]
+    [(let1E var val body)
+     (let ([new-env (extend nv
+                            var
+                            (interp val nv))])
+       (interp body new-env))]))
+
+
+; (parse: (s-exp -> Exp))
 (define (parse s)
   (cond
     [(s-exp-number? s)
@@ -42,7 +69,7 @@
     [else (error 'parse "unknown expression")]))
 
 (define (run s)
-  (calc (parse s)))
+  (interp (parse s) mt-env))
 
 ; Test cases
 (test (run `1) (numV 1))
